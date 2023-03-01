0524eea06cba91119d454de48719f0e7e6c71436
Katilho <pemicama12@gmail.com>
2023-03-01 11:04:34 +0000

    parte do id

---
+++ TPC3/addId.py
@@ -0,0 +1,14 @@
+import json
+
+mapa = {}
+with open("dataset-extra1.json", "r") as f:
+    mapa = json.load(f)
+
+idt = 0
+
+for p in mapa["pessoas"]:
+    p["id"] = idt
+    idt += 1
+
+with open("dataset-result.json", "w") as f:
+    json.dump(mapa, f)
